61e813541b1a6081e789267948d02c7a00a11658
Adrien Vauthey <adrien.vauthey@epfl.ch>
2023-05-28 17:45:00 +0200

    updated navbar and added smoother transition

--- website/js/custom.js
+++ website/js/custom.js
@@ -4,3 +4,16 @@ $('#siteNav').affix({
 		top: 100
 	}
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+	var navLinks = document.querySelectorAll('nav a');
+  
+	navLinks.forEach(function(link) {
+	  link.addEventListener('click', function(e) {
+		e.preventDefault();
+		var targetId = this.getAttribute('href');
+		var targetElement = document.querySelector(targetId);
+		targetElement.scrollIntoView({ behavior: 'smooth' });
+	  });
+	});
+  });
